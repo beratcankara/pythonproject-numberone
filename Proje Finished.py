be605303fe5b985924399a60dae9f8731f2ed573
Berat Can Kara <68200299+beratcankara@users.noreply.github.com>
2022-05-27 10:05:59 +0300

    Update Proje Finished.py

--- Proje Finished.py
+++ Proje Finished.py
@@ -1,4 +1,4 @@
-
+import sqlite3 as sql
 islem = """
 (1)Amiral battı oyunu
 (2)Hesap Makinası
@@ -7,7 +7,16 @@ islem = """
 """
 
 print(islem)
-
+isimi = input("Lutfen isminizi giriniz: ")
+def veritabani(isim, islemi):
+    vt = sql.connect("C:/Users/Berat Can Kara/Documents/veritabani.db")
+    im = vt.cursor()
+    olustur = """CREATE TABLE IF NOT EXISTS Kullanicilar (isimler,islemler)"""
+    ekle = """INSERT INTO Kullanicilar VALUES (?,?)"""
+    im.execute(olustur)
+    im.execute(ekle,(isim,islemi))
+    vt.commit()
+    vt.close()
 while True:
     islem = input("Yapmak istediğiniz işlemin numarasını seçiniz: \n[Çıkmak için 4. seçeneği seçiniz]\n")
     if islem == "4":
@@ -17,88 +26,90 @@ while True:
         print("Hakkımızda bölümüne yönlendiriliyorsunuz...")
         print("""
               instagram: https://www.instagram.com/mgorkemklc/
-             
+
               github: https://github.com/mgk27exe
-             
+
               linkedin: https://www.linkedin.com/in/m-g-k-197524211/
 
               youtube: https://www.youtube.com/channel/UCqa4-8vB3GQD7cuVN7DOBBw
-             
+
               spotify: https://open.spotify.com/user/e5afmyz44tqikxp3w7hzsb14w
-              
+
               NFT satış: https://opensea.io/Saint_Alcala
-              
+
               steam: https://steamcommunity.com/id/mgking27/
-              
+
               """)
     elif islem == "2":
         print("Hesap Makinesi Açılıyor...\n")
-        
+
         while True:
 
-       
             def toplama(a, b):
                 return a + b
-         
-       
+
+
             def cikarma(a, b):
                 return a - b
-         
-        
+
+
             def carpma(a, b):
                 return a * b
-         
-       
+
+
             def bolme(a, b):
                 return a / b
-         
+
+
             print("Bir işlem seçiniz...")
             print("Toplama işlemi için 1'e basınız.")
             print("Çıkarma işlemi için 2'ye basınız.")
             print("Çarpma işlemi için 3'e basınız.")
             print("Bölme işlemi için 4'e basınız.")
-         
+
             secim = input("Hangi işlemi yapmak istersiniz (1 - 2 - 3 - 4):")
-         
-        
+
             sayi1 = int(input("Birinci sayıyı giriniz = "))
             sayi2 = int(input("İkinci sayıyı giriniz = "))
-         
-        
+
             if secim == '1':
-                print(sayi1,"+",sayi2,"=", toplama(sayi1,sayi2))
-         
+                print(sayi1, "+", sayi2, "=", toplama(sayi1, sayi2))
+
             elif secim == '2':
-                print(sayi1,"-",sayi2,"=", cikarma(sayi1,sayi2))
-         
+                print(sayi1, "-", sayi2, "=", cikarma(sayi1, sayi2))
+
             elif secim == '3':
-                print(sayi1,"*",sayi2,"=", carpma(sayi1,sayi2))
-         
+                print(sayi1, "*", sayi2, "=", carpma(sayi1, sayi2))
+
             elif secim == '4':
-                print(sayi1,"/",sayi2,"=", bolme(sayi1,sayi2))
+                print(sayi1, "/", sayi2, "=", bolme(sayi1, sayi2))
             else:
                 print("Değer girilmedi.")
-        
+
             tercih = input("Başka işlem yapmak istiyor musunuz ? (E/H) \n")
             if tercih == 'E':
                 break
-        
+
     elif islem == "1":
         print("Amiral battı oyunu açılıyor...")
-    
-         
+
         from random import randint
+
         board = []
         sayac = 0
         puan = 250
         for i in range(5):
-            board.append(["0"]*5)
+            board.append(["0"] * 5)
+
 
         def print_board(board):
             for satir in board:
-                print (" ".join(satir))
+                print(" ".join(satir))
+
+
         def rand(board):
-            return randint(1,len(board)-1)
+            return randint(1, len(board) - 1)
+
 
         print("-" * 35)
         print("Amiral batti oyununa hosgeldiniz! -Oyun yapimcisi M.Gorkem Kilic ")
@@ -115,7 +126,7 @@ while True:
         gemi2_sutun = rand(board)
 
         while True:
-            if(gemi_satir == gemi1_satir and gemi_sutun == gemi1_sutun):
+            if (gemi_satir == gemi1_satir and gemi_sutun == gemi1_sutun):
                 gemi1_satir = rand(board)
                 gemi1_sutun = rand(board)
                 continue
@@ -134,9 +145,9 @@ while True:
                 tercih = input("Başka işlem yapmak istiyor musunuz ? (E/H) \n")
                 if tercih == 'E':
                     break
-                if (tahmin_satir == gemi_satir and tahmin_sutun == gemi_sutun)\
-                    or (tahmin_satir == gemi1_satir and tahmin_sutun == gemi1_sutun) \
-                    or (tahmin_satir == gemi2_satir and tahmin_sutun == gemi2_sutun):
+                if (tahmin_satir == gemi_satir and tahmin_sutun == gemi_sutun) \
+                        or (tahmin_satir == gemi1_satir and tahmin_sutun == gemi1_sutun) \
+                        or (tahmin_satir == gemi2_satir and tahmin_sutun == gemi2_sutun):
                     if board[tahmin_satir - 1][tahmin_sutun - 1] == "/":
                         print("-" * 35)
                         print("Zaten tahmin ettiniz")
@@ -146,12 +157,12 @@ while True:
                         print("-" * 35)
                         print("Tebrikler gemiyi batirdiniz!")
                         board[tahmin_satir - 1][tahmin_sutun - 1] = "/"
-                        print("Puaniniz:",puan)
+                        print("Puaniniz:", puan)
                         print("-" * 35)
                         print_board(board)
                         sayac += 1
                 else:
-                    if (tahmin_satir < 0 or tahmin_sutun < 0) or (tahmin_satir >5 or tahmin_sutun >5):
+                    if (tahmin_satir < 0 or tahmin_sutun < 0) or (tahmin_satir > 5 or tahmin_sutun > 5):
                         print("-" * 35)
                         print("Alan sÄ±nÄ±rlarÄ± dÄ±ÅÄ±nda deÄer girdiniz")
 
@@ -174,33 +185,6 @@ while True:
                         print("Tebrikler bÃ¼tÃ¼n gemileri batÄ±rdÄ±nÄ±z ve oyunu kazandÄ±nÄ±z")
                         print("-" * 35)
                         break
-                    
-                    
-                        
     else:
         print("LÜTFEN GEÇERLİ İŞLEM NUMARASI GİRİNİZ...")
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
+veritabani(isimi,islem)
